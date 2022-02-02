fb8c05a51dea969bf3cb45ef1ddcaada2384e0c5
averdin2 <alexverdin01@gmail.com>
2022-02-02 14:20:58 -0800

    Fixed styles for line logo

--- src/components/StationPrediction.js
+++ src/components/StationPrediction.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/Display.css';
+import '../styles/StationPrediction.css';
 
 export default function StationPrediction(props) {
   const { metroLogoClass, stationTitle, times } = props;
@@ -7,8 +8,13 @@ export default function StationPrediction(props) {
   return (
     <tr className="table-row table-body-text">
       <td>
-        <span className={`line-logo ${metroLogoClass}`}></span>
-        {stationTitle}
+        <div className="line-title">
+          <div className="logo-container">
+            {/* <span className={`line-logo ${metroLogoClass}`}></span> */}
+            <div className={`line-logo ${metroLogoClass}`}></div>
+          </div>
+          <div className="title-container">{stationTitle}</div>
+        </div>
       </td>
       <td>{timesString}</td>
     </tr>
